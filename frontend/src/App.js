3a9c1d0af9e1b64372d9069d1ee45ef0d29de612
theBappy <abappy575@gmail.com>
2025-09-28 13:52:57 +0600

    start fronted with page and instance of axios api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
-
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import UserLogin from "./pages/auth/login";
 
 function App() {
   return (
-    <div className="App text-green-400 bg-black">
-      Hello World
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path="/user-login" element={<UserLogin />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
